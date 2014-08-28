64636ec92d21c67a281e13bcc435f2940cc3b2f8
John Bickar <jbickar@stanford.edu>
2014-08-28 13:21:58 -0700

    Making map link field purty

---
+++ js/stanford_event_nodes.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+
+  $('.node-stanford-event .field-name-field-s-event-map-link a').prepend('<i class="icon-map-marker"></i> ');
+  $('.node-stanford-event .field-name-field-s-event-map-link a').addClass('btn btn-sm');
+});
+
